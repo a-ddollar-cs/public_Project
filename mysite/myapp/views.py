1cd05390164ae0c5609374ed79b873e7e2172d58
Austin Dollar <addollar@mail.csuchico.edu>
2020-09-24 16:53:41 -0700

    My Title and Content

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -3,4 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("CINS465 Hello World")
+    title = "My Title"
+    content = "Content"
+    context = {
+        "title":title,
+        "body":content
+    }
+
+    return render(request, "base.html", context=context)
--- test.py
+++
@@ -1,6 +0,0 @@
-x = 3
-x = 3.0
-x = Truex = "three"
-
-
-print(x)
