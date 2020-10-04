29e1eafaa351cdb2eb80ac4cd3cbb0da0d2150d3
Austin Dollar <addollar@mail.csuchico.edu>
2020-10-04 14:20:07 -0700

    finish lecture 6

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 class SuggestionModel(models.Model):
     suggestion = models.CharField(max_length = 240)
+
+
+    def __str__(self):
+        return self.suggestion
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . import models
+
 # Create your views here.
 def index(request):
-    return render(request, "base.html")
+    title = "Assignment 4"
+    suggestions = models.SuggestionModel.objects.all()
+    context = {
+        "title": title,
+        "suggestions":suggestions
+        
+    }
+    return render(request, "index.html", context = context)
 
 
 def page(request):
